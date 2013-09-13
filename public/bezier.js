1fee25a195c37f726c457b5c024cbd943f4e3820
adrian <adrian@localhost.localdomain>
2013-09-13 17:42:33 +0100

    anim complete

--- public/bezier.js
+++ public/bezier.js
@@ -1,38 +1,67 @@
 
 
-function bezier(globals){
+function bezier(globals, position){
 
 
     this.globals = globals;
+    this.position = position;
     this.indexes = {}    
     this.indexes['start'] = {};
     this.indexes['end'] = {};
     this.indexes['cp1'] = {};
     this.indexes['cp2'] = {};
-
+    this.boundary = 3;
+    this.behaviourDelay = 200;
 
     this.init = function(){ 
          
-        var axis = Math.floor((Math.random()*this.globals.c1.width)+1);
-        var arch = 200;
+        console.log('initialising bezier at position ' + this.position);
+         
+        this.phase = 0;
+        this.phaseSinMultiplier = 120;
+        this.phaseCoSinMultiplier = 120;
+        this.speed = 10;       
+        this.color = "RGB(255,255,255)";
+        this.width = 0.1;
+        this.behaviour = 0;
+        this.tick = 0;
 
-        this.indexes['start']['x'] = axis; // position width ways
-        this.indexes['start']['y'] = -1; // from top
-        this.indexes['end']['x'] = axis;
-        this.indexes['end']['y'] = this.globals.c1.height + 1; // from top
-        this.indexes['cp1']['x'] = axis + arch; 
-        this.indexes['cp1']['y'] = this.globals.c1.height / 4 * 1; 
-        this.indexes['cp2']['x'] = axis + arch; 
-        this.indexes['cp2']['y'] = this.globals.c1.height / 4 * 3; 
+         
+        // initilaize bezier in position, default is box 1
+        switch(this.position){
 
-
-        this.speed = Math.floor((Math.random()*100)+1);
-        this.color = '#fff';
-        this.width = Math.floor((Math.random()*3)+1);
-        this.inverted = false;   
-        this.animation = Math.floor((Math.random()*2)+1);
-        //this.animation = 3;
-        this.ttl = Math.floor((Math.random()*100)+1);
+            // top left to bottom right
+            case 1:
+            default:
+                this.indexes['start']['x'] = 0; // position width ways
+                this.indexes['start']['y'] = 0; // from top        
+                this.indexes['end']['x'] = this.globals.c1.width;
+                this.indexes['end']['y'] = this.globals.c1.height; // from top
+            break;
+            // bottom left to top right
+            case 2:
+                this.indexes['start']['x'] = this.globals.c1.width; // position width ways
+                this.indexes['start']['y'] = 0; // from top        
+                this.indexes['end']['x'] = 0;
+                this.indexes['end']['y'] = this.globals.c1.height; // from top
+            break;
+            // vertical
+            case 3:
+                this.indexes['start']['x'] = this.globals.c1.width/2; // position width ways
+                this.indexes['start']['y'] = 0; // from top        
+                this.indexes['end']['x'] = this.globals.c1.width/2;
+                this.indexes['end']['y'] = this.globals.c1.height; // from top
+            break;
+            // horizontal
+            case 4:                
+                this.indexes['start']['x'] = 0; // position width ways
+                this.indexes['start']['y'] = this.globals.c1.height/2; // from top        
+                this.indexes['end']['x'] = this.globals.c1.width;
+                this.indexes['end']['y'] = this.globals.c1.height/2; // from top
+            break;
+                
+        }
+        
 
      }
 
@@ -55,57 +84,119 @@ function bezier(globals){
         return this;
      }
 
-     this.animate = function(){
-
-        if (this.ttl<1){
-            this.init();
-            return;
-        } else {
-            this.ttl--;
-        }
-
-        switch(this.animation){
-             
-             case 1:                  
-                this.indexes['cp1']['y']+= this.speed;
-                this.indexes['cp1']['x']+= this.speed; 
-                this.indexes['cp2']['x']+= this.speed; 
-                this.indexes['start']['x']-= this.speed;  
-                if (this.speed>5 && this.ttl<100){
-                    this.speed--;
-                }
-            break;             
-            case 2:                 
-                this.indexes['cp1']['y']-= this.speed;
-                this.indexes['cp1']['x']-= this.speed; 
-                this.indexes['cp2']['x']-= this.speed; 
-                this.indexes['end']['x']-= this.speed;  
-                if (this.speed>5 && this.ttl<100){
-                    this.speed--;
-                }
-            break;
-            case 3:                  
-                this.indexes['start']['y']-= this.speed;
-                this.indexes['cp1']['x']+= this.speed; 
-                this.indexes['cp2']['x']+= this.speed; 
-                this.indexes['end']['x']+= this.speed;  
-                if (this.speed>5 && this.ttl<100){
-                    this.speed--;
-                }
-            break;             
-            case 4:                 
-                this.indexes['start']['y']+= this.speed;
-                this.indexes['cp1']['x']+= this.speed; 
-                this.indexes['cp2']['x']+= this.speed; 
-                this.indexes['end']['x']+= this.speed; 
-            break;     
-             
+     this.calcBounds = function(value){
+         
+         // box 1
+         if (value['x'] >= this.boundary && value['y'] <= this.boundary){
+             if (value['x'] > this.globals.c1.width - this.boundary){
+                 value['y']+= this.speed;                 
+             } else {
+                value['x']+= this.speed;  
+             }
+         }
+         // box 2
+         else if (value['x'] >= this.globals.c1.width - this.boundary && value['y'] >= this.boundary){
+             if (value['y'] > this.globals.c1.height - this.boundary){
+                value['x']-= this.speed;
+             } else {
+                value['y']+= this.speed;
+             }             
+         }
+         // box 3
+         else if (value['x'] <= this.globals.c1.width - this.boundary && value['y'] >= this.globals.c1.height - this.boundary){      
+             if (value['x'] < this.boundary){
+                 value['y']-= this.speed;                 
+             } else {
+                value['x']-= this.speed;  
+             }
+         }
+         // box 4
+         else {
+             if (value['y'] < this.boundary){
+                value['x']+= this.speed;
+             } else {
+                value['y']-= this.speed;
+             }
          }
          
-         return this;
+                  
+         return value;
      }
 
-     this.update = function(){
+     
+     this.animate = function(){
+
+        this.phase+=0.09;        
+        this.indexes['start'] = this.calcBounds(this.indexes['start']);
+        this.indexes['end'] = this.calcBounds(this.indexes['end']);        
+        this.indexes['cp1']['x'] = this.globals.c1.width/2 + Math.cos(this.phase) * this.phaseCoSinMultiplier;  
+        this.indexes['cp1']['y'] = this.globals.c1.height/2 + Math.sin(this.phase) * this.phaseSinMultiplier; 
+        this.indexes['cp2']['x'] = this.globals.c1.width/2 + Math.cos(this.phase) * this.phaseCoSinMultiplier;  
+        this.indexes['cp2']['y'] = this.globals.c1.height/2 + Math.sin(this.phase) * this.phaseSinMultiplier;  
+        return this;
+     }
+     
+     this.applyBehaviour = function(){
+         
+         if (this.tick%this.behaviourDelay==1){        
+             this.behaviour++;
+             console.log('behaviour change '+ this.behaviour);
+        }
+        
+        switch(this.behaviour){
+      
+            case 1: 
+                if (this.speed >5){
+                    this.speed--; 
+                }
+                if (this.width<0.5){
+                    this.width+=.01;    
+                }
+                break;
+            case 2: 
+                
+                if (this.speed <20){
+                    this.speed++; 
+                }  
+                if (this.width>0.3){
+                    this.width-=.01; 
+                }
+                break;
+            case 3: 
+                if (this.speed <10){
+                    this.speed++; 
+                }
+                if (this.phaseSinMultiplier < 500){
+                    this.phaseSinMultiplier++; 
+                }
+                if (this.phaseCoSinMultiplier < 500){
+                    this.phaseCoSinMultiplier++; 
+                }
+                if (this.width<0.7){
+                    this.width+=.01;    
+                }
+                break;
+            case 4: 
+                if (this.phaseSinMultiplier > 120){
+                    this.phaseSinMultiplier--; 
+                }
+                if (this.phaseCoSinMultiplier > 120){
+                    this.phaseCoSinMultiplier--; 
+                }
+                if (this.width>0.5){
+                    this.width-=.01; 
+                }
+                break;
+            default:
+                this.behaviour = 1;
+      
+        }
+     }
+
+     this.update = function(){          
+         
+         this.tick++;
+         this.applyBehaviour();
          this.animate();
          return this;
      }
